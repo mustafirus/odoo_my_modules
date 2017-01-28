df9334b960846d0dc28bc50860a93ded59eb64a3
mustafirus <golubev@nt.net.ua>
2017-01-28 22:23:48 +0200

    helpdesk scafold

---
+++ helpdesk/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "HelpDesk",
+    'version': "0.1",
+    'author': "Golubev",
+    'category': "Tools",
+    'support': "golubev@svami.in.ua",
+    'summary': "A helpdesk / support ticket system",
+    'description': "A helpdesk / support ticket system",
+    'license':'LGPL-3',
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    'demo': [],
+    'depends': ['mail'],
+    'installable': True,
+}
