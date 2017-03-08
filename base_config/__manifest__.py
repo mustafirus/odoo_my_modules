966a8b38954947ebf80ad5a6977489b0a0505575
mustafirus <golubev@nt.net.ua>
2017-03-08 09:07:54 +0200

    base config 	new file:   base_config/__init__.py 	new file:   base_config/__manifest__.py 	new file:   base_config/data/config.xml 	new file:   base_config/models/__init__.py 	new file:   base_config/models/config.py

---
+++ base_config/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "base_config",
+    'category': 'base',
+    'version': '0.1',
+    'summary': """
+        Configure mail and other base settings""",
+    'description': """
+        Configure mail and other base settings
+    """,
+    'author': "golubev",
+    'website': "http://www.cloud.net.ua",
+    'depends': ['mail'],
+    'data': [
+        'data/config.xml',
+    ],
+}
---
+++ base_config/models/config.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+
+class BaseConfig(models.TransientModel):
+    _inherit = 'base.config.settings'
+
+    @api.model
+    def _set_default_mail_catchall_domain(self):
+        alias_domain = "erp.ssc.bla" #{{odoo_domain}}
+        mail_server= self.env['ir.mail_server'].sudo().search([('name', '=', 'localhost')])
+        fetchmail_server= self.env['fetchmail.server'].sudo().search([('name', '=', 'localhost')])
+
+        # mail_server.create(
+        #     {'name': "svami", 'smtp_host': "mail.svami.in.ua", 'smtp_port': "587",
+        #      'smtp_user': mailbox + "@odoo.svami.in.ua",
+        #      'smtp_encryption': "starttls"}
+        # )
+        if len(fetchmail_server) == 0:
+            fetchmail_server.create(
+                {'name': "localhost", 'server': "localhost", 'port': "143",
+                 'type': 'imap', 'is_ssl': False, 'user': "odoo", 'password' : "ffh"} # {{odoo_mail_password}}
+            )
+        self.env['ir.config_parameter'].set_param("mail.catchall.domain", alias_domain)
+
+
