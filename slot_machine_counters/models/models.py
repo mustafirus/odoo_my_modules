7dbc0a347e79eef0b02fc6d5dbcb781e6813b62b
mustafirus <golubev@nt.net.ua>
2017-07-17 01:27:40 +0300

    maint w/o tab in report 	modified:   slot_machine_counters/models/models.py 	modified:   slot_machine_counters/views/hallslot.xml 	modified:   slot_machine_counters/views/slotshot.xml

--- slot_machine_counters/models/models.py
+++ slot_machine_counters/models/models.py
@@ -6,8 +6,11 @@ import json
 import urllib2
 import werkzeug.urls
 
+def get_now():
+    return fields.datetime.now().replace(second=0, microsecond=0).strftime(fields.DATETIME_FORMAT)
 
-def get_data_rrd(devid, date_beg=lambda self: fields.datetime.now(), date_end=lambda self: fields.datetime.now()):
+
+def get_data_rrd(devid, date_beg, date_end):
     GAMBLING_ENDPOINT = 'http://localhost:4000/counters'
     GAMBLING_ENDPOINT = 'http://rrd.odoo.bla:4000/counters?'
     headers = {"Content-type": "application/x-www-form-urlencoded"}
@@ -93,7 +96,7 @@ class Slot(models.Model):
     currency_id = fields.Many2one('res.currency', related='hall_id.company_id.currency_id', readonly=True,
         help='Utility field to express amount currency')
     hall_id = fields.Many2one("slot_machine_counters.hall","Hall")
-    repair_slotshot_id = fields.Many2one('slot_machine_counters.slotshot.line', string='Repair Slotshot')
+    maintshot_id = fields.Many2one('slot_machine_counters.maintshot.line', string='Maintenance Slotshot')
     active = fields.Boolean('Active?', default=True)
 
     @api.depends('boardrate','denom')
@@ -103,25 +106,24 @@ class Slot(models.Model):
 
     @api.multi
     def repair_begin(self):
-        slotline = self.env['slot_machine_counters.slotshot.line']
-        rrd = get_data_rrd(self.dev_sn)
-        self.repair_slotshot_id = slotline.create({
+#        slotline = self.env['slot_machine_counters.maintshot.line']
+#        rrd = get_data_rrd(self.dev_sn)
+        self.maintshot_id = self.maintshot_id.create({
             'slot_id': self.id,
-            'index':   self.index,
-            'iin_beg': rrd['iinB'] if rrd['iinB'] else rrd['betB'],
-            'out_beg': rrd['outB'] if rrd['outB'] else rrd['winB'],
-            'bet_beg': rrd['betB'],
-            'bet_end': rrd['betE'],
-            'win_beg': rrd['winB'],
-            'win_end': rrd['winE'],
+            'date_beg': get_now(),
         })
+        return
 
     @api.multi
     def repair_end(self):
-        rrd = get_data_rrd(self.dev_sn)
-        self.repair_slotshot_id.write({
+        date_beg = self.maintshot_id.date_beg
+        date_end = get_now()
+        rrd = get_data_rrd(self.dev_sn, date_beg, date_end)
+        self.maintshot_id.write({
             'slot_id': self.id,
             'index':   self.index,
+            'date_beg': date_beg,
+            'date_end': date_end,
             'iin_beg': rrd['iinB'] if rrd['iinB'] else rrd['betB'],
             'iin_end':   rrd['iinE'] if rrd['iinE'] else rrd['betE'],
             'out_beg': rrd['outB'] if rrd['outB'] else rrd['winB'],
@@ -131,10 +133,10 @@ class Slot(models.Model):
             'win_beg': rrd['winB'],
             'win_end': rrd['winE'],
         })
-        self.repair_slotshot_id = None
+        self.maintshot_id = None
         return
 
-            # @api.depends('dev_sn')
+    # @api.depends('dev_sn')
     # def _compute_name(self):
     #     for rec in self:
     #         rec.name = rec.dev_sn if rec.dev_sn else "Unknown"
@@ -146,7 +148,7 @@ class SlotShot(models.Model):
 
     @api.model
     def _get_now(self):
-        return fields.datetime.now().replace(second=0, microsecond=0).strftime(fields.DATETIME_FORMAT)
+        return get_now()
 
     name = fields.Char("Name", readonly=True)
     hall_id = fields.Many2one("slot_machine_counters.hall","Hall", required=True)
@@ -312,6 +314,82 @@ class SlotShotLine(models.Model):
 #################
 
 
+class MaintShotLine(models.Model):
+    _name = 'slot_machine_counters.maintshot.line'
+    _description = 'MaintShot Line'
+
+    @api.model
+    def _get_now(self):
+        return get_now()
+
+
+    date_beg = fields.Datetime("From", required=True, default=_get_now)
+    date_end = fields.Datetime("To", required=True, default=_get_now)
+
+    slot_id   = fields.Many2one("slot_machine_counters.slot","Slot")
+    index     = fields.Integer("Index")
+    iin_beg   = fields.Integer("in")
+    iin_end   = fields.Integer("IN")
+    out_beg   = fields.Integer("out")
+    out_end   = fields.Integer("OUT")
+    iin       = fields.Integer("IN-in",compute='_compute_iin', readonly=True, store=True)
+    out       = fields.Integer("OUT-out",compute='_compute_out', readonly=True, store=True)
+    credit    = fields.Integer("Credit",compute='_compute_credit', readonly=True, store=True)
+    amount    = fields.Monetary("Cash",compute='_compute_amount', readonly=True, store=True)
+    bet_beg   = fields.Integer("bet")
+    bet_end   = fields.Integer("BET")
+    win_beg   = fields.Integer("win")
+    win_end   = fields.Integer("WIN")
+    bet       = fields.Integer("BET-bet",compute='_compute_bet', readonly=True, store=True)
+    win       = fields.Integer("WIN-win",compute='_compute_win', readonly=True, store=True)
+    credit_bw = fields.Integer("Credit",compute='_compute_credit_bw', readonly=True, store=True)
+    amount_bw = fields.Monetary("Cash",compute='_compute_amount_bw', readonly=True, store=True)
+    currency_id = fields.Many2one('res.currency', related='slot_id.hall_id.company_id.currency_id', readonly=True,
+        help='Utility field to express amount currency')
+#################
+    @api.depends('iin_beg','iin_end')
+    def _compute_iin(self):
+        for rec in self:
+            rec.iin = (rec.iin_end - rec.iin_beg)
+
+    @api.depends('out_beg','out_end')
+    def _compute_out(self):
+        for rec in self:
+            rec.out = (rec.out_end - rec.out_beg)
+
+    @api.depends('iin','out')
+    def _compute_credit(self):
+        for rec in self:
+            rec.credit = rec.iin - rec.out
+
+    @api.depends('credit')
+    def _compute_amount(self):
+        for rec in self:
+            rec.amount = rec.credit * rec.slot_id.denomenation
+#################
+
+    @api.depends('bet_beg','bet_end')
+    def _compute_bet(self):
+        for rec in self:
+            rec.bet = (rec.bet_end - rec.bet_beg)
+
+    @api.depends('win_beg','win_end')
+    def _compute_win(self):
+        for rec in self:
+            rec.win = (rec.win_end - rec.win_beg)
+
+    @api.depends('bet','win')
+    def _compute_credit_bw(self):
+        for rec in self:
+            rec.credit_bw = rec.bet - rec.win
+
+    @api.depends('credit_bw')
+    def _compute_amount_bw(self):
+        for rec in self:
+            rec.amount_bw = rec.credit_bw * rec.slot_id.denomenation
+#################
+
+
 class HallReport(models.TransientModel):
     _name = 'slot_machine_counters.hallreport'
     _description = 'HallReport'
@@ -321,6 +399,7 @@ class HallReport(models.TransientModel):
     date_end = fields.Datetime("To", required=True)
     full = fields.Boolean("Full report", default=False)
     hallreport_lines = fields.One2many('slot_machine_counters.hallreport.line', 'hallreport_id', string='Slotshot Lines')
+    hallreport_maint = fields.One2many('slot_machine_counters.hallreport.maint', 'hallreport_id', string='Slotshot Lines')
     # credit     = fields.Integer("Credit",compute='_compute_total', readonly=True, store=True)
     amount     = fields.Monetary("Cash",compute='_compute_total', readonly=True, store=True)
     # credit_bw  = fields.Integer("Credit",compute='_compute_total', readonly=True, store=True)
@@ -378,6 +457,10 @@ class HallReport(models.TransientModel):
 
     @api.multi
     def print_rep(self, shots):
+        line_model=self.env['slot_machine_counters.hallreport.line']
+        line_model.search([('hallreport_id', '=',  self.id)]).unlink()
+        maint_model=self.env['slot_machine_counters.hallreport.maint']
+        maint_model.search([('hallreport_id', '=',  self.id)]).unlink()
         self.env.cr.execute("""
             SELECT slot_id, string_agg(distinct cast(index as text), ','),
             min(iin_beg) as iin_beg, max(iin_end) as iin_end, sum(iin) as iin,
@@ -388,40 +471,106 @@ class HallReport(models.TransientModel):
             sum(credit_bw) as credit_bw, sum(amount_bw) as amount_bw
             FROM slot_machine_counters_slotshot_line where slotshot_id in %s group by slot_id ;
         """, (tuple(shots.ids),))
-        res = self.env.cr.fetchall()
-        print res
-        line_model=self.env['slot_machine_counters.hallreport.line']
-        line_model.search([('hallreport_id', '=',  self.id)]).unlink()
+        line_res = self.env.cr.fetchall()
+        print line_res
+        slots = zip(*line_res)[0]
 
-        for line in res:
+        self.env.cr.execute("""
+            SELECT slot_id, string_agg(distinct cast(index as text), ','),
+            min(iin_beg) as iin_beg, max(iin_end) as iin_end, sum(iin) as iin,
+            min(out_beg) as out_beg, max(out_end) as out_end, sum(out) as out,
+            sum(credit) as credit, sum(amount) as amount,
+            min(bet_beg) as bet_beg, max(bet_end) as bet_end, sum(bet) as bet,
+            min(win_beg) as win_beg, max(win_end) as win_end, sum(win) as win,
+            sum(credit_bw) as credit_bw, sum(amount_bw) as amount_bw
+            FROM slot_machine_counters_maintshot_line where slot_id in %s and date_beg >= %s and date_end < %s
+            group by slot_id ;
+        """, (tuple(slots), self.date_beg, self.date_end, ))
+        maint_res = self.env.cr.fetchall()
+        print maint_res
+        maint_dict = dict(map(lambda x: (x[0],x),maint_res))
+
+        for line in line_res:
+            maint = maint_dict.get(line[0], (line[0], u'', 0, 0, 0, 0, 0, 0, 0, 0.0, 0, 0, 0, 0, 0, 0, 0, 0.0))
             vals = {
                 'hallreport_id':  self.id,
                 'slot_id':    line[0],
                 'index': line[1],
                 'iin_beg': line[2],
                 'iin_end': line[3],
-                'iin': line[4],
+                'iin': line[4] - maint[4],
                 'out_beg': line[5],
                 'out_end': line[6],
-                'out': line[7],
-                'credit': line[8],
-                'amount': line[9],
+                'out': line[7] - maint[7],
+                'credit': line[8] - maint[8],
+                'amount': line[9] - maint[9],
                 'bet_beg': line[10],
                 'bet_end': line[11],
-                'bet': line[12],
+                'bet': line[12] - maint[12],
                 'win_beg': line[13],
                 'win_end': line[14],
-                'win': line[15],
-                'credit_bw': line[16],
-                'amount_bw': line[17],
+                'win': line[15] - maint[15],
+                'credit_bw': line[16] - maint[16],
+                'amount_bw': line[17] - maint[17],
             }
             line_model.create(vals)
+
+        for maint in maint_res:
+            vals = {
+                'hallreport_id':  self.id,
+                'slot_id':    maint[0],
+                'index': maint[1],
+                'iin_beg': maint[2],
+                'iin_end': maint[3],
+                'iin': maint[4] - maint[4],
+                'out_beg': maint[5],
+                'out_end': maint[6],
+                'out': maint[7] - maint[7],
+                'credit': maint[8] - maint[8],
+                'amount': maint[9] - maint[9],
+                'bet_beg': maint[10],
+                'bet_end': maint[11],
+                'bet': maint[12] - maint[12],
+                'win_beg': maint[13],
+                'win_end': maint[14],
+                'win': maint[15] - maint[15],
+                'credit_bw': maint[16] - maint[16],
+                'amount_bw': maint[17] - maint[17],
+            }
+            maint_model.create(vals)
 
         return self.env['report'].get_action(self, 'slot_machine_counters.hallreport_multi')
 
 class HallReportLine(models.TransientModel):
     _name = 'slot_machine_counters.hallreport.line'
     _description = 'HallReport Line'
+    _order = 'hallreport_id,index'
+
+    hallreport_id = fields.Many2one('slot_machine_counters.hallreport', string='HallReport Reference', required=True, ondelete='cascade', index=True, copy=False)
+    index     = fields.Char("Index")
+    slot_id   = fields.Many2one("slot_machine_counters.slot","Slot")
+    iin_beg   = fields.Integer()
+    iin_end   = fields.Integer()
+    out_beg   = fields.Integer()
+    out_end   = fields.Integer()
+    iin       = fields.Integer(readonly=True, store=True)
+    out       = fields.Integer(readonly=True, store=True)
+    credit = fields.Integer(readonly=True, store=True)
+    amount = fields.Monetary(readonly=True, store=True)
+    bet_beg   = fields.Integer()
+    bet_end   = fields.Integer()
+    win_beg   = fields.Integer()
+    win_end   = fields.Integer()
+    bet       = fields.Integer(readonly=True, store=True)
+    win       = fields.Integer(readonly=True, store=True)
+    credit_bw = fields.Integer(readonly=True, store=True)
+    amount_bw = fields.Monetary(readonly=True, store=True)
+    currency_id = fields.Many2one('res.currency', related='hallreport_id.hall_id.company_id.currency_id', readonly=True,
+        help='Utility field to express amount currency')
+
+class HallReportMaint(models.TransientModel):
+    _name = 'slot_machine_counters.hallreport.maint'
+    _description = 'HallReport Maint'
     _order = 'hallreport_id,index'
 
     hallreport_id = fields.Many2one('slot_machine_counters.hallreport', string='HallReport Reference', required=True, ondelete='cascade', index=True, copy=False)
