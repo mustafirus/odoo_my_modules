d630efb3f3df6368da02b335b0ef0f0f09390f0f
mustafirus <golubev@nt.net.ua>
2018-02-04 16:14:31 +0200

    website portal begin work

--- omixbilling/controllers/controllers.py
+++ omixbilling/controllers/controllers.py
@@ -1,20 +1,65 @@
 # -*- coding: utf-8 -*-
-from odoo import http
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-# class Omixbilling(http.Controller):
-#     @http.route('/omixbilling/omixbilling/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+from collections import OrderedDict
 
-#     @http.route('/omixbilling/omixbilling/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('omixbilling.listing', {
-#             'root': '/omixbilling/omixbilling',
-#             'objects': http.request.env['omixbilling.omixbilling'].search([]),
-#         })
+from odoo import http, _
+from odoo.addons.website_portal.controllers.main import website_account
+from odoo.http import request
 
-#     @http.route('/omixbilling/omixbilling/objects/<model("omixbilling.omixbilling"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('omixbilling.object', {
-#             'object': obj
-#         })
+
+class WebsiteAccount(website_account):
+
+    def _prepare_portal_layout_values(self):
+        values = super(WebsiteAccount, self)._prepare_portal_layout_values()
+        # domain is needed to hide non portal project for employee
+        # portal users can't see the privacy_visibility, fetch the domain for them in sudo
+        subscription_count = request.env['omixbilling.subscription'].search_count([])
+        values.update({
+            'subscription_count': subscription_count,
+        })
+        return values
+
+    @http.route(['/my/subscriptions', '/my/subscriptions/page/<int:page>'], type='http', auth="user", website=True)
+    def my_subscriptions(self, page=1, date_begin=None, date_end=None, project=None, sortby=None, **kw):
+        values = self._prepare_portal_layout_values()
+
+        sortings = {
+            'date': {'label': _('Newest'), 'order': 'create_date desc'},
+            'name': {'label': _('Name'), 'order': 'name'},
+        }
+
+        domain = ([])
+        order = sortings.get(sortby, sortings['date'])['order']
+
+        # archive groups - Default Group By 'create_date'
+        archive_groups = self._get_archive_groups('omixbilling.subscription', [('active', '=', False)])
+        # pager
+        pager = request.website.pager(
+            url="/my/subscriptions",
+            url_args={'date_begin': date_begin, 'date_end': date_end},
+            total=values['subscription_count'],
+            page=page,
+            step=self._items_per_page
+        )
+        # content according to pager and archive selected
+        subscriptions = request.env['omixbilling.subscription'].search(domain, order=order, limit=self._items_per_page, offset=pager['offset'])
+
+        values.update({
+            'date': date_begin,
+            'date_end': date_end,
+            'sortings': sortings,
+            'sortby': sortby,
+            'subscriptions': subscriptions,
+            'page_name': 'subscription',
+            'archive_groups': archive_groups,
+            'default_url': '/my/subscriptions',
+            'pager': pager
+        })
+        return request.render("omixbilling.my_subscriptions", values)
+
+    @http.route(['/my/subscriptions/<int:subscription_id>'], type='http', auth="user", website=True)
+    def my_subscriptions_subscription(self, subscription_id=None, **kw):
+        subscription = request.env['omixbilling.subscription'].browse(subscription_id)
+        return request.render("omixbilling.my_subscriptions_subscription", {'subscription': subscription})
+
